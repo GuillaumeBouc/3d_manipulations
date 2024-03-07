1c8528228b854dcea006f10d0040145b5076416f
GuillaumeBouc <guillaumeboucomont@gmail.com>
2024-03-07 10:23:14 +0100

    Oh De La Couleur !

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from debug import debug
 from Shape import *
 from matrix import *
 
@@ -10,9 +11,11 @@ class App:
         self.fps = 60
         self.screen = pygame.display.set_mode(self.resolution)
         self.clock = pygame.time.Clock()
+        self.camera_vector = [0, 0, 1]
 
     def draw(self, shape: Shape):
-        self.screen.fill((0, 0, 0))
+        a = 0
+        self.screen.fill((255, 255, 255))
         for face in shape.faces.values():
             for triangle in face.triangles:
                 projected_points = calculate_projection(
@@ -28,45 +31,41 @@ class App:
                     (point[0] * 200 + self.width / 2, point[1] * 200 + self.height / 2)
                     for point in projected_points
                 ]
-                pygame.draw.polygon(
-                    self.screen,
-                    (255, 255, 255),
-                    projected_points,
-                    2,
-                )
+                if np.dot(triangle.normal, self.camera_vector) < 0:
+                    a += 1
+                    pygame.draw.polygon(
+                        self.screen,
+                        triangle.color,
+                        projected_points,
+                    )
+                    pygame.draw.polygon(
+                        self.screen,
+                        (0, 0, 0),
+                        projected_points,
+                        5,
+                    )
 
     def calculate(self, shape: Shape, rotation: list[str], angle: float) -> Shape:
-        rotated_faces = []
+
         for face in shape.faces.values():
-            rotated_triangles = []
+
             for triangle in face.triangles:
-                rotated_vertices = calculate_rotations(
+                triangle.v1, triangle.v2, triangle.v3 = calculate_rotations(
                     [triangle.v1, triangle.v2, triangle.v3], rotation, angle
                 )
-
-                rotated_triangles.append(
-                    Triangle(
-                        rotated_vertices[0],
-                        rotated_vertices[1],
-                        rotated_vertices[2],
-                    )
-                )
-            rotated_faces.append(Face(rotated_triangles))
-        faces_dict = {
-            name: face for name, face in zip(shape.faces.keys(), rotated_faces)
-        }
-        return Shape(faces_dict)
+                triangle.normal = triangle.calculate_normal()
 
     def run(self, shape: Shape):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    sys.quit()
+                    sys.exit()
             pygame.display.set_caption(
                 "First Test (FPS: " + str(round(self.clock.get_fps(), 2)) + ")"
             )
-            shape = self.calculate(shape, ["X", "Y", "Z"], 0.01)
+
+            self.calculate(shape, ["X", "Y", "Z"], 0.01)
             self.draw(shape)
             pygame.display.flip()
             self.clock.tick(self.fps)
--- Shape.py
+++ Shape.py
@@ -2,10 +2,33 @@ import numpy as np
 
 
 class Triangle:
-    def __init__(self, v1: list[int], v2: list[int], v3: list[int]) -> None:
+    def __init__(
+        self, v1: list[int], v2: list[int], v3: list[int], color: list[int]
+    ) -> None:
         self.v1 = v1
         self.v2 = v2
         self.v3 = v3
+        self.normal = self.calculate_normal()
+        self.color = color
+
+    def calculate_normal(self) -> list[float]:
+        # Get the first three vertices
+
+        v1 = np.array(self.v1, np.float64)
+        v2 = np.array(self.v2, np.float64)
+        v3 = np.array(self.v3, np.float64)
+
+        # Calculate two edge vectors
+        edge1 = v2 - v1
+        edge2 = v3 - v1
+
+        # Calculate the cross product to get the normal vector
+        normal = np.cross(edge1, edge2)
+
+        # Normalize the normal vector
+        normal /= np.linalg.norm(normal)
+        normal = normal.tolist()
+        return normal
 
 
 class Face:
@@ -16,119 +39,104 @@ class Face:
 class Shape:
     def __init__(self, faces: dict[str, Face]) -> None:
         self.faces = faces
-        self.normals = self.calculate_normals()
-
-    def calculate_normals(self) -> list[list[float]]:
-        normals = []
-        for face in self.faces.values():
-            for triangle in face.triangles:
-                normal = self.calculate_normal(triangle)
-                normals.append(normal)
-        return normals
-
-    def calculate_normal(self, face):
-        # Get the first three vertices
-        v1 = np.array(face[0], np.float64)
-        v2 = np.array(face[1], np.float64)
-        v3 = np.array(face[2], np.float64)
-
-        # Calculate two edge vectors
-        edge1 = v2 - v1
-        edge2 = v3 - v1
-
-        # Calculate the cross product to get the normal vector
-        normal = np.cross(edge1, edge2)
-
-        # Normalize the normal vector
-        normal /= np.linalg.norm
-        normal = normal.tolist()
-        return normal
 
 
 def make_cube():
     return Shape(
         {
-            "1/2": Face(
+            "1": Face(
                 [
                     Triangle(
-                        [-1 / 2, -1 / 2, -1 / 2],
-                        [-1 / 2, 1 / 2, -1 / 2],
-                        [1 / 2, 1 / 2, -1 / 2],
+                        [-1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [255, 187, 141],
                     ),
                     Triangle(
-                        [-1 / 2, -1 / 2, -1 / 2],
-                        [1 / 2, 1 / 2, -1 / 2],
-                        [1 / 2, -1 / 2, -1 / 2],
+                        [-1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [255, 187, 141],
                     ),
                 ]
             ),
             "2": Face(
                 [
                     Triangle(
-                        [1 / 2, -1 / 2, -1 / 2],
-                        [1 / 2, 1 / 2, -1 / 2],
-                        [1 / 2, 1 / 2, 1 / 2],
+                        [1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [255, 254, 141],
                     ),
                     Triangle(
-                        [1 / 2, -1 / 2, -1 / 2],
-                        [1 / 2, 1 / 2, 1 / 2],
-                        [1 / 2, -1 / 2, 1 / 2],
+                        [1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [255, 254, 141],
                     ),
                 ]
             ),
             "3": Face(
                 [
                     Triangle(
-                        [1 / 2, -1 / 2, 1 / 2],
-                        [1 / 2, 1 / 2, 1 / 2],
-                        [-1 / 2, 1 / 2, 1 / 2],
+                        [1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [255, 141, 141],
                     ),
                     Triangle(
-                        [1 / 2, -1 / 2, 1 / 2],
-                        [-1 / 2, 1 / 2, 1 / 2],
-                        [-1 / 2, -1 / 2, 1 / 2],
+                        [1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [255, 141, 141],
                     ),
                 ]
             ),
             "4": Face(
                 [
                     Triangle(
-                        [-1 / 2, -1 / 2, 1 / 2],
-                        [-1 / 2, 1 / 2, 1 / 2],
-                        [-1 / 2, 1 / 2, -1 / 2],
+                        [-1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [141, 255, 145],
                     ),
                     Triangle(
-                        [-1 / 2, -1 / 2, 1 / 2],
-                        [-1 / 2, 1 / 2, -1 / 2],
-                        [-1 / 2, -1 / 2, -1 / 2],
+                        [-1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [-1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [141, 255, 145],
                     ),
                 ]
             ),
             "5": Face(
                 [
                     Triangle(
-                        [-1 / 2, 1 / 2, -1 / 2],
-                        [-1 / 2, 1 / 2, 1 / 2],
-                        [1 / 2, 1 / 2, 1 / 2],
+                        [-1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [-1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [226, 141, 255],
                     ),
                     Triangle(
-                        [-1 / 2, 1 / 2, -1 / 2],
-                        [1 / 2, 1 / 2, 1 / 2],
-                        [1 / 2, 1 / 2, -1 / 2],
+                        [-1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, 1 / 2 + 0.5],
+                        [1 / 2, 1 / 2, -1 / 2 + 0.5],
+                        [226, 141, 255],
                     ),
                 ]
             ),
             "6": Face(
                 [
                     Triangle(
-                        [1 / 2, -1 / 2, 1 / 2],
-                        [-1 / 2, -1 / 2, 1 / 2],
-                        [-1 / 2, -1 / 2, -1 / 2],
+                        [1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [146, 141, 255],
                     ),
                     Triangle(
-                        [1 / 2, -1 / 2, 1 / 2],
-                        [-1 / 2, -1 / 2, -1 / 2],
-                        [1 / 2, -1 / 2, -1 / 2],
+                        [1 / 2, -1 / 2, 1 / 2 + 0.5],
+                        [-1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [1 / 2, -1 / 2, -1 / 2 + 0.5],
+                        [146, 141, 255],
                     ),
                 ]
             ),
